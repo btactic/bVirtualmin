63108ff390396c6c4ae993da3824794940eb3451
Marc Sánchez <sanchezfauste@gmail.com>
2016-12-13 13:50:33 +0100

    Adding Scheduler to cal bVirtualmin sync

---
+++ custom/Extension/modules/Schedulers/Ext/Language/en_us.bVirtualmin.php
@@ -0,0 +1,3 @@
+<?php
+
+$mod_strings['LBL_BVIRTUALMIN'] = 'bVirtualmin Synchronization';
---
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/bVirtualmin.php
@@ -0,0 +1,8 @@
+<?php
+
+$job_strings[] = 'bVirtualmin';
+
+function bVirtualmin() {
+    require_once('bVirtualmin/main.php');
+    return main();
+}
