19fdd5e03bc287e11a8a13f30127a4e1a216dbd6
Marc Sánchez <sanchezfauste@gmail.com>
2018-06-20 18:32:48 +0200

    Pase virtualservers output locally

--- bVirtualmin/virtualmin_api.php
+++ bVirtualmin/virtualmin_api.php
@@ -3,11 +3,15 @@
 abstract class VirtualminAPI {
 
     private static $get_virtualservers_script = 'bVirtualmin/scripts/get_virtualservers.sh';
+    private static $parse_virtualservers_script = 'bVirtualmin/scripts/parse_virtualservers.sh';
     private static $virtualmin_servers_config_file = 'bVirtualmin/config/virtualmin_servers.ini';
 
     static public function get_virtualservers_of_virtualmin($virtualmin_server) {
-        $virtualservers_info = self::execute_command_on_server($virtualmin_server,
-                "$(cat ".self::$get_virtualservers_script.")");
+        $virtualservers_info = self::execute_command_on_server(
+            $virtualmin_server,
+            "$(cat ".self::$get_virtualservers_script.")",
+            file_get_contents(self::$parse_virtualservers_script)
+        );
         $virtualservers = array();
         foreach ($virtualservers_info as $virtualserver) {
             $virtualserver = json_decode($virtualserver);
@@ -16,9 +20,12 @@ abstract class VirtualminAPI {
         return $virtualservers;
     }
 
-    static private function execute_command_on_server($virtualmin_server, $cmd) {
+    static private function execute_command_on_server($virtualmin_server, $cmd,
+            $local_piped_command = '') {
         exec("ssh -p ".$virtualmin_server['port']." ".$virtualmin_server['user']
-                ."@".$virtualmin_server['host']." \"".$cmd."\"", $result);
+            ."@".$virtualmin_server['host']." \"".$cmd."\""
+            .(empty($local_piped_command) ? '' : " | $local_piped_command"), $result
+        );
         return $result;
     }
 
